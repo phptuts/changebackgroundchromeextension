4f0ba624362e181359df0f362bc9b57f83cfc64e
phptuts <glaserpower@gmail.com>
2021-06-15 23:29:16 -0700

    Fixed the chrome extension

--- popup.js
+++ popup.js
@@ -6,33 +6,44 @@ const rotate3DEl = document.getElementById("rotate3d");
 
 colorEl.addEventListener("input", (e) => {
   const hexValue = e.target.value;
-  chrome.tabs.executeScript(null, {
-    code: `document.body.style.backgroundColor='${hexValue}'`,
-  });
+  injectCSS(`body {
+    background-color: ${hexValue}!important;
+  }`);
 });
 
 rotateEl.addEventListener("input", (e) => {
   const degree = e.target.value;
-  
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var currTab = tabs[0];
-    if (currTab) { // Sanity check
-      chrome.scripting.insertCSS({
-        css: `body { transform: rotate(${degree}deg) !important; }`,
-        target: {tabId: currTab.id}
-      }, function() {
-        console.log('called');
-        if (chrome.runtime.lastError) {
-          message.innerText = 'There was an error injecting css : \n' + chrome.runtime.lastError.message;
-        }
-      });
-    }
-  });
+  injectCSS(`body { 
+    transform: rotate(${degree}deg) !important; 
+  }`);
 });
 
 rotate3DEl.addEventListener("input", (e) => {
   const degree = e.target.value;
-  chrome.tabs.executeScript(null, {
-    code: `document.body.style.transform='rotate3d(1, 1, 1,${degree}deg)'`,
-  });
+  injectCSS(`body { 
+    transform: rotate3d(1, 1, 1,${degree}deg) !important; 
+  }`);
 });
+
+function injectCSS(css) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    var currTab = tabs[0];
+    if (currTab) {
+      // Sanity check
+      chrome.scripting.insertCSS(
+        {
+          css,
+          target: { tabId: currTab.id },
+        },
+        function () {
+          console.log("called");
+          if (chrome.runtime.lastError) {
+            message.innerText =
+              "There was an error injecting css : \n" +
+              chrome.runtime.lastError.message;
+          }
+        }
+      );
+    }
+  });
+}
